138e31eff6e6b7e417a388fbd2b609fbe7076e11
Ruslan Piskarev <ruslan.piskarev@gmail.com>
2013-03-31 15:38:16 +0300

    dumper

---
+++ mamont/dumper/ses.php
@@ -0,0 +1,31 @@
+<?php
+$SES = array (
+  '894bec664ef01a81d216430b9d31e318' => 
+  array (
+    'cfg' => 
+    array (
+      'charsets' => 'cp1251 utf8 latin1',
+      'lang' => 'ru',
+      'time_web' => '600',
+      'time_cron' => '600',
+      'backup_path' => 'backup/',
+      'backup_url' => 'backup/',
+      'only_create' => 'MRG_MyISAM MERGE HEAP MEMORY',
+      'globstat' => 0,
+      'my_host' => 'localhost',
+      'my_port' => 3306,
+      'my_user' => 'ortoline_orto',
+      'my_pass' => 'zenkov77',
+      'my_comp' => 0,
+      'my_db' => 'ortoline_zinkov4',
+      'auth' => 'mysql cfg',
+      'user' => '',
+      'pass' => '',
+      'confirm' => '6',
+      'exitURL' => './',
+    ),
+    'time' => 1356419083,
+    'lng' => 'ru',
+  ),
+);
+?>
