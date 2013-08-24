8e4ecef59d81da06192bce3e529d605c04e56390
Ruslan Piskarev <ruslan.piskarev@gmail.com>
2013-08-24 15:58:55 +0300

    #37: Глобальный поиск - финиш.

--- module/FcLibrariesSearch/src/FcLibrariesSearch/Model/SearchModel.php
+++ module/FcLibrariesSearch/src/FcLibrariesSearch/Model/SearchModel.php
@@ -45,6 +45,12 @@ class SearchModel extends AbstractTableGateway
                 $select->order('reg_number ' . Select::ORDER_ASCENDING);
                 break;
 
+            case 'library_aircraft_type':
+                $select->columns(array('id', 'name'));
+                $select->where->like('name', $text . '%');
+                $select->order('name ' . Select::ORDER_ASCENDING);
+                break;
+
             case 'library_air_operator':
                 $select->columns(array('id', 'name', 'short_name', 'code_icao', 'code_iata'));
                 $select->join(array('library_country' => 'library_country'),
@@ -61,25 +67,69 @@ class SearchModel extends AbstractTableGateway
                 break;
 
             case 'library_airport':
-                $fields = array('name', 'short_name', 'code_icao', 'code_iata');
-                break;
-
-            case 'library_country':
-                $fields = array('name', 'code');
-                break;
-
-            case 'library_currency':
-                $fields = array('name', 'currency');
-                break;
-
-            case 'library_kontragent':
-                $fields = array('name', 'short_name', 'sita');
+                $select->columns(array('id', 'name', 'short_name', 'code_icao', 'code_iata'));
+                $select->join(array('city' => 'library_city'),
+                    'library_airport.city_id = city.id',
+                    array('city_name' => 'name'), 'left');
+                $select->join(array('country' => 'library_country'),
+                    'city.country_id = country.id',
+                    array('country_name' => 'name'), 'left');
+                $select->join(array('region' => 'library_region'),
+                    'country.region_id = region.id',
+                    array('region_name' => 'name'), 'left');
+                $select->where->like('library_airport.name', $text . '%')
+                    ->or
+                    ->like('library_airport.short_name', $text . '%')
+                    ->or
+                    ->like('library_airport.code_icao', $text . '%')
+                    ->or
+                    ->like('library_airport.code_iata', $text . '%');
+                $select->order('name ' . Select::ORDER_ASCENDING);
                 break;
 
             case 'library_city':
+                $select->columns(array('id', 'name', 'country_id'));
+                $select->join(array('country' => 'library_country'),
+                    'country.id = library_city.country_id',
+                    array('country_name' => 'name'), 'left');
+                $select->where->like('library_city.name', $text . '%');
+                $select->order('name ' . Select::ORDER_ASCENDING);
+                break;
+
+            case 'library_country':
+                $select->columns(array('id', 'name', 'code'));
+                $select->join(array('region' => 'library_region'),
+                    'region.id = library_country.region_id',
+                    array('region_name' => 'name'), 'left');
+                $select->where->like('library_country.name', $text . '%')
+                    ->or
+                    ->like('library_country.code', $text . '%');
+                $select->order('name ' . Select::ORDER_ASCENDING);
+                break;
+
+            case 'library_currency':
+                $select->columns(array('id', 'name', 'currency'));
+                $select->where->like('name', $text . '%');
+                $select->order('name ' . Select::ORDER_ASCENDING);
+                break;
+
+            case 'library_kontragent':
+                $select->columns(array('id',
+                    'name', 'short_name', 'address', 'phone1', 'phone2', 'phone3', 'fax', 'mail', 'sita'));
+                $select->where->like('name', $text . '%');
+                $select->order('name ' . Select::ORDER_ASCENDING);
+                break;
+
             case 'library_region':
+                $select->columns(array('id', 'name'));
+                $select->where->like('name', $text . '%');
+                $select->order('name ' . Select::ORDER_ASCENDING);
+                break;
+
             case 'library_unit':
-                $fields = array('name');
+                $select->columns(array('id', 'name'));
+                $select->where->like('name', $text . '%');
+                $select->order('name ' . Select::ORDER_ASCENDING);
                 break;
 
             default:
