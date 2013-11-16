8b9a56e97701a8bf7a65207dd8a7519a57244330
Ruslan Piskarev <ruslan.piskarev@gmail.com>
2013-11-16 10:36:44 +0200

    #68: AP Service - фильтр и валидация на стороне сервера.

--- module/FcFlight/src/FcFlight/Filter/ApServiceFilter.php
+++ module/FcFlight/src/FcFlight/Filter/ApServiceFilter.php
@@ -35,15 +35,29 @@ class ApServiceFilter implements InputFilterAwareInterface
     //Real fields
     public $id;
     public $headerId;
+    public $legId;
     public $airportId;
-    public $isNeed;
+    public $typeOfApServiceId;
     public $agentId;
+    public $price;
+    public $currency;
+    public $exchangeRate;
+    public $priceUSD;
 
     //Virtual fields
     public $icao;
     public $iata;
     public $airportName;
+    public $typeOfApServiceName;
     public $kontragentShortName;
+
+    /**
+     * @var array
+     */
+    protected $defaultFilters = array(
+        array('name' => 'StripTags'),
+        array('name' => 'StringTrim'),
+    );
 
     /**
      * @param \Zend\Db\Adapter\Adapter $dbAdapter
@@ -71,14 +85,20 @@ class ApServiceFilter implements InputFilterAwareInterface
         //Real fields
         $this->id = (isset($data['id'])) ? $data['id'] : null;
         $this->headerId = (isset($data['headerId'])) ? $data['headerId'] : null;
+        $this->legId = (isset($data['legId'])) ? $data['legId'] : null;
         $this->airportId = (isset($data['airportId'])) ? $data['airportId'] : null;
-        $this->isNeed = (isset($data['isNeed'])) ? $data['isNeed'] : null;
+        $this->typeOfApServiceId = (isset($data['typeOfApServiceId'])) ? $data['typeOfApServiceId'] : null;
         $this->agentId = (isset($data['agentId'])) ? $data['agentId'] : null;
+        $this->price = (isset($data['price'])) ? $data['price'] : null;
+        $this->currency = (isset($data['currency'])) ? $data['currency'] : null;
+        $this->exchangeRate = (isset($data['exchangeRate'])) ? $data['exchangeRate'] : null;
+        $this->priceUSD = (isset($data['priceUSD'])) ? $data['priceUSD'] : null;
 
         //Virtual fields
         $this->icao = (isset($data['icao'])) ? $data['icao'] : null;
         $this->iata = (isset($data['iata'])) ? $data['iata'] : null;
         $this->airportName = (isset($data['airportName'])) ? $data['airportName'] : null;
+        $this->typeOfApServiceName = (isset($data['$typeOfApServiceName'])) ? $data['$typeOfApServiceName'] : null;
         $this->kontragentShortName = (isset($data['kontragentShortName'])) ? $data['kontragentShortName'] : null;
     }
 
@@ -114,21 +134,106 @@ class ApServiceFilter implements InputFilterAwareInterface
             $inputFilter->add($factory->createInput(array(
                 'name' => 'headerId',
                 'required' => true,
+                'filters' => array(
+                    array('name' => 'Int'),
+                ),
+            )));
+
+            $inputFilter->add($factory->createInput(array(
+                'name' => 'legId',
+                'required' => true,
+                'filters' => array(
+                    array('name' => 'Int'),
+                ),
             )));
 
             $inputFilter->add($factory->createInput(array(
                 'name' => 'airportId',
                 'required' => true,
+                'filters' => array(
+                    array('name' => 'Int'),
+                ),
             )));
 
             $inputFilter->add($factory->createInput(array(
-                'name' => 'isNeed',
-                'required' => false,
+                'name' => 'typeOfApServiceId',
+                'required' => true,
+                'filters' => array(
+                    array('name' => 'Int'),
+                ),
             )));
 
             $inputFilter->add($factory->createInput(array(
                 'name' => 'agentId',
                 'required' => true,
+                'filters' => array(
+                    array('name' => 'Int'),
+                ),
+            )));
+
+            $inputFilter->add($factory->createInput(array(
+                'name' => 'price',
+                'required' => true,
+                'filters' => $this->defaultFilters,
+                'validators' => array(
+                    array(
+                        'name' => 'StringLength',
+                        'options' => array(
+                            'encoding' => 'UTF-8',
+                            'min' => 16,
+                        ),
+                    ),
+                    array(
+                        'name' => 'Float',
+                    ),
+                ),
+            )));
+
+            $inputFilter->add($factory->createInput(array(
+                'name' => 'currency',
+                'required' => true,
+                'filters' => $this->defaultFilters,
+                'validators' => array(
+                    array(
+                        'name' => 'NotEmpty',
+                    ),
+                ),
+            )));
+
+            $inputFilter->add($factory->createInput(array(
+                'name' => 'exchangeRate',
+                'required' => true,
+                'filters' => $this->defaultFilters,
+                'validators' => array(
+                    array(
+                        'name' => 'StringLength',
+                        'options' => array(
+                            'encoding' => 'UTF-8',
+                            'min' => 8,
+                        ),
+                    ),
+                    array(
+                        'name' => 'Float',
+                    ),
+                ),
+            )));
+
+            $inputFilter->add($factory->createInput(array(
+                'name' => 'priceUSD',
+                'required' => true,
+                'filters' => $this->defaultFilters,
+                'validators' => array(
+                    array(
+                        'name' => 'StringLength',
+                        'options' => array(
+                            'encoding' => 'UTF-8',
+                            'min' => 8,
+                        ),
+                    ),
+                    array(
+                        'name' => 'Float',
+                    ),
+                ),
             )));
 
             $this->inputFilter = $inputFilter;
