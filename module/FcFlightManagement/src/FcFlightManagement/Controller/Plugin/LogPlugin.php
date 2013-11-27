2def6c4a3ed9b02360d70d8a108a28e61fbb6dbf
Ruslan Piskare <ruslan.piskarev@gmail.com>
2013-11-27 10:23:00 +0200

    #78: hot fix

--- module/FcFlightManagement/src/FcFlightManagement/Controller/Plugin/LogPlugin.php
+++ module/FcFlightManagement/src/FcFlightManagement/Controller/Plugin/LogPlugin.php
@@ -7,12 +7,13 @@
 namespace FcFlightManagement\Controller\Plugin;
 
 use Zend\Mvc\Controller\Plugin\AbstractPlugin;
+use FcFlight\Controller\Plugin\LogPlugin as FcFlightLogPlugin;
 
 /**
  * Class LogPlugin
  * @package FcLibraries\Controller\Plugin
  */
-class LogPlugin extends AbstractPlugin
+class LogPlugin extends FcFlightLogPlugin
 {
 
 }
