3f8b8c75da6ea40dd6f086fa54afb71628bfbc5e
Ruslan Piskarev <ruslan.piskarev@gmail.com>
2013-12-11 12:59:28 +0200

    #85: Management Refuel - Outcome Invoice - подготовка.

--- module/FcFlightManagement/src/FcFlightManagement/Controller/RefuelController.php
+++ module/FcFlightManagement/src/FcFlightManagement/Controller/RefuelController.php
@@ -8,14 +8,8 @@ use FcFlightManagement\Form\RefuelIncomeInvoiceStep1Form;
 use Zend\Mvc\Controller\AbstractActionController;
 use Zend\View\Model\ViewModel;
 use FcFlight\Controller\FlightController;
-use FcFlight\Model\FlightHeaderModel;
-use FcFlight\Model\LegModel;
-use FcFlight\Model\PermissionModel;
 use FcFlightManagement\Model\RefuelModel;
-use FcFlight\Model\ApServiceModel;
 use FcFlight\Form\ApServiceForm;
-use FcFlightManagement\Model\RefuelIncomeInvoiceMainModel;
-use FcFlightManagement\Model\RefuelIncomeInvoiceDataModel;
 
 /**
  * Class RefuelController
@@ -50,6 +44,16 @@ class RefuelController extends FlightController
      * @var \FcFlightManagement\Model\RefuelIncomeInvoiceDataModel
      */
     protected $incomeInvoiceRefuelDataModel;
+
+    /**
+     * @var \FcFlightManagement\Model\RefuelOutcomeInvoiceMainModel
+     */
+    protected $outcomeInvoiceRefuelMainModel;
+
+    /**
+     * @var \FcFlightManagement\Model\RefuelOutcomeInvoiceDataModel
+     */
+    protected $outcomeInvoiceRefuelDataModel;
 
 
     /**
@@ -220,6 +224,178 @@ class RefuelController extends FlightController
         ));
 
     }
+
+    /**
+     * @return ViewModel
+     */
+    public function outcomeInvoiceStep1Action()
+    {
+        $result = array();
+        $searchForm = new RefuelIncomeInvoiceStep1Form('refuelIncomeInvoiceStep1',
+            array(
+                'libraries' => array(
+                    'aircrafts' => $this->getAircrafts(),
+                    'agents' => $this->getKontragents(),
+                    'airports' => $this->getAirports(),
+                    'customers' => $this->getKontragents(),
+                    'airOperators' => $this->getAirOperators(),
+                )
+            )
+        );
+
+        $request = $this->getRequest();
+
+        if ($request->isPost()) {
+            $data = $request->getPost();
+
+            $postIsEmpty = true;
+            foreach ($this->_mapFields as $field) {
+                if (isset($data->$field) && !empty($data->$field)) {
+                    $postIsEmpty = false;
+                    continue;
+                }
+            }
+
+            if ($postIsEmpty) {
+                $this->flashMessenger()->addErrorMessage('Result not found. Enter one or more fields.');
+                return $this->redirect()->toRoute('management/refuel/income-invoice-step1');
+            }
+
+            $filter = $this->getServiceLocator()->get('FcFlightManagement\Filter\RefuelIncomeInvoiceStep1Filter');
+            $searchForm->setInputFilter($filter->getInputFilter());
+
+            $searchForm->setData($request->getPost());
+            if ($searchForm->isValid() && !$postIsEmpty) {
+                $data = $searchForm->getData();
+                $result = $this->getRefuelModel()->findByParams($data);
+
+            }
+        }
+
+        return array(
+            'form' => $searchForm,
+            'result' => $result,
+        );
+    }
+
+    /**
+     * @return ViewModel
+     */
+    public function outcomeInvoiceStep2Action()
+    {
+        $result = array();
+        $units = array();
+        $unitsObj = $this->getUnits();
+        foreach ($unitsObj as $unit) {
+            $units[$unit->id] = $unit->name;
+        }
+
+        $currencies = new ApServiceForm(null, array());
+        $currencies = $currencies->getCurrencyExchangeRate();
+
+        $request = $this->getRequest();
+        if ($request->isPost()) {
+            $data = $request->getPost();
+
+            if (empty($data['refuelsSelected'])) {
+                $this->flashMessenger()->addErrorMessage('Result not found. Enter one or more fields.');
+                return $this->redirect()->toRoute('management/refuel/income-invoice-step1');
+            }
+
+            $result = $this->getRefuelModel()->findByParams($data);
+        }
+
+        return array(
+            'currencies' => $currencies,
+            'units' => $units,
+            'result' => $result,
+        );
+    }
+
+    /**
+     * @return ViewModel
+     */
+    public function outcomeInvoiceStep3Action()
+    {
+        $units = array();
+        $unitsObj = $this->getUnits();
+        foreach ($unitsObj as $unit) {
+            $units[$unit->id] = $unit->name;
+        }
+
+        $currencies = new ApServiceForm(null, array());
+        $currencies = $currencies->getCurrencyExchangeRate();
+
+        $request = $this->getRequest();
+
+        if ($request->isPost()) {
+            $result = $request->getPost();
+
+            return array(
+                'currencies' => $currencies,
+                'units' => $units,
+                'result' => $result,
+            );
+        }
+
+        return $this->redirect()->toRoute('management/refuel/income-invoice-step1');
+    }
+
+    /**
+     * @return mixed
+     */
+    public function outcomeInvoiceAddAction()
+    {
+        $request = $this->getRequest();
+        if ($request->isPost()) {
+            $data = $request->getPost();
+//            \Zend\Debug\Debug::dump($data);
+            $invoiceId = $this->getIncomeInvoiceRefuelMainModel()->add($data);
+
+            foreach ($data['data'] as $row) {
+                $row['invoiceId'] = $invoiceId;
+                $row['preInvoiceRefuelId'] = $row['refuelId'];
+                $this->getIncomeInvoiceRefuelDataModel()->add($row);
+            }
+
+            $message = "Refuel income invoice was successfully added.";
+            $this->flashMessenger()->addSuccessMessage($message);
+
+            return $this->redirect()->toRoute('management/refuel/income-invoice-show',
+                array(
+                    'id' => $invoiceId,
+                ));
+        }
+
+        return $this->redirect()->toRoute('management/refuel/income-invoice-step1');
+    }
+
+    /**
+     * @return ViewModel
+     */
+    public function outcomeInvoiceShowAction()
+    {
+        $invoiceId = (string)$this->params()->fromRoute('id', '');
+
+        if (empty($invoiceId)) {
+            return $this->redirect()->toRoute('management/refuel/income-invoice-step1');
+        }
+
+        $header = $this->getIncomeInvoiceRefuelMainModel()->get($invoiceId);
+        $data = $this->getIncomeInvoiceRefuelDataModel()->getByInvoiceId($invoiceId);
+
+        foreach ($data as $key => $row) {
+            $header->data[$row->refuelId] = $row;
+        }
+
+        return new ViewModel(array(
+            'header' => $header,
+        ));
+
+    }
+
+
+
 
     /**
      * Get Refuel model
