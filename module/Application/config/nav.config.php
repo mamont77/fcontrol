04fca9692bb2cdea9c51f1906aae5b2d979b3962
Ruslan Piskarev <ruslan.piskarev@gmail.com>
2014-02-25 12:35:01 +0200

    Management // Структура навигации #159

--- module/Application/config/nav.config.php
+++ module/Application/config/nav.config.php
@@ -21,28 +21,99 @@ return array(
                 'route' => 'management',
                 'pages' => array(
                     array(
-                        'label' => 'Refuel income',
-                        'route' => 'management/refuel/income-invoice-step1',
+                        'label' => 'INCOME INVOICES',
+                        'type' => 'uri',
+                        'navHeader' => true,
                     ),
                     array(
-                        'label' => 'Refuel outcome',
-                        'route' => 'management/refuel/outcome-invoice-step1',
+                        'label' => 'Refuel income',
+                        'route' => 'management/refuel/income-invoice-step1',
                     ),
                     array(
                         'label' => 'AP Service income',
                         'route' => 'management/ap-service/income-invoice-step1',
                     ),
                     array(
-                        'label' => 'AP Service outcome',
-                        'route' => 'management/ap-service/outcome-invoice-step1',
-                    ),
-                    array(
                         'label' => 'Permission income',
                         'route' => 'management/permission/income-invoice-step1',
                     ),
                     array(
+                        'label' => 'Mistake invoices',
+                        'type' => 'uri',
+                    ),
+                    array(
+                        'label' => 'Credit invoice',
+                        'type' => 'uri',
+                    ),
+                    array(
+                        'label' => 'Registry of income invoices',
+                        'type' => 'uri',
+                    ),
+                    array(
+                        'label' => 'OUTCOME INVOICES',
+                        'type' => 'uri',
+                        'navHeader' => true,
+                    ),
+                    array(
+                        'label' => 'Refuel outcome',
+                        'route' => 'management/refuel/outcome-invoice-step1',
+                    ),
+
+                    array(
+                        'label' => 'AP Service outcome',
+                        'route' => 'management/ap-service/outcome-invoice-step1',
+                    ),
+
+                    array(
                         'label' => 'Permission outcome',
                         'route' => 'management/permission/outcome-invoice-step1',
+                    ),
+                    array(
+                        'label' => 'Credit invoice',
+                        'type' => 'uri',
+                    ),
+                    array(
+                        'label' => 'Registry of income invoices',
+                        'type' => 'uri',
+                    ),
+                    array(
+                        'label' => 'BANK ACTIVITY',
+                        'type' => 'uri',
+                        'navHeader' => true,
+                    ),
+                    array(
+                        'label' => '...',
+                        'type' => 'uri',
+                    ),
+                    array(
+                        'label' => '...',
+                        'type' => 'uri',
+                    ),
+                    array(
+                        'label' => 'ACCOUNT STATEMENTS',
+                        'type' => 'uri',
+                        'navHeader' => true,
+                    ),
+                    array(
+                        'label' => '...',
+                        'type' => 'uri',
+                    ),
+                    array(
+                        'label' => '...',
+                        'type' => 'uri',
+                    ),
+                    array(
+                        'label' => 'FINANCIAL REPORT',
+                        'type' => 'uri',
+                        'navHeader' => true,
+                    ),
+                    array(
+                        'label' => '...',
+                        'type' => 'uri',
+                    ),
+                    array(
+                        'label' => '...',
+                        'type' => 'uri',
                     ),
                 ),
             ),
@@ -78,10 +149,10 @@ return array(
                         'label' => 'Countries',
                         'route' => 'zfcadmin/countries',
                     ),
-                    array(
-                        'label' => 'Currencies',
-                        'route' => 'zfcadmin/currencies',
-                    ),
+//                    array(
+//                        'label' => 'Currencies',
+//                        'route' => 'zfcadmin/currencies',
+//                    ),
                     array(
                         'label' => 'Kontragents',
                         'route' => 'zfcadmin/kontragents',
